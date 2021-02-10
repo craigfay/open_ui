acde8942939dcde73022c584c43b18bca81e9cfb
Craig Fay <craigfay1@gmail.com>
2021-02-10 08:38:08 -0800

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,27 @@
+fn main() {
+    use glium::glutin;
+
+    let mut event_loop = glutin::event_loop::EventLoop::new();
+    let wb = glutin::window::WindowBuilder::new();
+    let cb = glutin::ContextBuilder::new();
+    let display = glium::Display::new(wb, cb, &event_loop).unwrap();
+
+    event_loop.run(move |ev, _, control_flow| {
+
+        let frame_duration = std::time::Duration::from_nanos(16_666_667);
+        let next_frame_time = std::time::Instant::now() + frame_duration;
+        *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
+
+        match ev {
+            glutin::event::Event::WindowEvent { event, .. } => match event {
+                glutin::event::WindowEvent::CloseRequested => {
+                    *control_flow = glutin::event_loop::ControlFlow::Exit;
+                    return;
+                },
+                _ => return,
+            },
+            _ => (),
+        }
+    });
+}
+
