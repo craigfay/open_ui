035f7f773ff25c3f9f12d8912d6dc1e017ed133e
craig fay <craigfay1@gmail.com>
2021-02-14 20:55:42 -0600

    Introduce shapes, shaders

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,59 @@
-fn main() {
-    use glium::glutin;
+#[macro_use]
+extern crate glium;
 
-    let mut event_loop = glutin::event_loop::EventLoop::new();
+fn main() {
+    #[allow(unused_imports)]
+    use glium::{glutin, Surface};
+
+    let event_loop = glutin::event_loop::EventLoop::new();
     let wb = glutin::window::WindowBuilder::new();
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
-    event_loop.run(move |ev, _, control_flow| {
+    #[derive(Copy, Clone)]
+    struct Vertex {
+        position: [f32; 2],
+    }
 
-        let frame_duration = std::time::Duration::from_nanos(16_666_667);
-        let next_frame_time = std::time::Instant::now() + frame_duration;
+    implement_vertex!(Vertex, position);
+
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [ 0.0,  0.5] };
+    let vertex3 = Vertex { position: [ 0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main() {
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main() {
+            color = vec4(1.0, 0.0, 0.0, 0.5);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
+    event_loop.run(move |event, _, control_flow| {
+        let next_frame_time = std::time::Instant::now() +
+            std::time::Duration::from_nanos(16_666_667);
+
         *control_flow = glutin::event_loop::ControlFlow::WaitUntil(next_frame_time);
 
-        match ev {
+        match event {
             glutin::event::Event::WindowEvent { event, .. } => match event {
                 glutin::event::WindowEvent::CloseRequested => {
                     *control_flow = glutin::event_loop::ControlFlow::Exit;
@@ -20,8 +61,25 @@ fn main() {
                 },
                 _ => return,
             },
-            _ => (),
+            glutin::event::Event::NewEvents(cause) => match cause {
+                glutin::event::StartCause::ResumeTimeReached { .. } => (),
+                glutin::event::StartCause::Init => (),
+                _ => return,
+            },
+            _ => return,
         }
+
+        let mut target = display.draw();
+        target.clear_color(0.0, 0.0, 1.0, 1.0);
+
+        target.draw(
+            &vertex_buffer,
+            &indices,
+            &program,
+            &glium::uniforms::EmptyUniforms,
+            &Default::default()
+        ).unwrap();
+
+        target.finish().unwrap();
     });
 }
-
